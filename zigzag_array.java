9ac59484c4b81944aa74004c2495062b003eaa95
Aakash <116252032+Aakashbalamurugan@users.noreply.github.com>
2026-06-23 23:27:40 +0530

    add zizzag arrag , day 2 leetcode

---
+++ zigzag_array.java
@@ -0,0 +1,44 @@
+class Solution {
+    private static final int MOD = 1_000_000_007;
+
+    public int zigZagArrays(int n, int l, int r) {
+        int m = r - l + 1;
+
+        long[] up = new long[m];
+        long[] down = new long[m];
+
+        for (int i = 0; i < m; i++) {
+            up[i] = i;             
+            down[i] = m - 1 - i;    
+        }
+
+        for (int len = 3; len <= n; len++) {
+            long[] nextUp = new long[m];
+            long[] nextDown = new long[m];
+
+            long[] prefUp = new long[m + 1];
+            long[] prefDown = new long[m + 1];
+
+            for (int i = 0; i < m; i++) {
+                prefUp[i + 1] = (prefUp[i] + up[i]) % MOD;
+                prefDown[i + 1] = (prefDown[i] + down[i]) % MOD;
+            }
+
+            for (int i = 0; i < m; i++) {
+                nextUp[i] = prefDown[i];
+
+                nextDown[i] = (prefUp[m] - prefUp[i + 1] + MOD) % MOD;
+            }
+
+            up = nextUp;
+            down = nextDown;
+        }
+
+        long ans = 0;
+        for (int i = 0; i < m; i++) {
+            ans = (ans + up[i] + down[i]) % MOD;
+        }
+
+        return (int) ans;
+    }
+}
